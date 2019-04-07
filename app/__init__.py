614edce4da8b1bd56f44412cb8721cf2f1d44abd
epan64 <ericpan64@ucla.edu>
2019-04-07 00:12:27 -0500

    Removing unnecessary files

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-# Initial tutorial: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-
-from flask import Flask
-from flask.ext.login import LoginManager
-
-app = Flask(__name__)
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from app import mFlask
-
-@app.route('/') # example of decorator (modifies function following @ variable)
-@app.route('/index')
-def index():
-    return "Hello, World!"
