a7655b9465f0d32feafc931dfd5f85667a1e48ab
ericpan64 <ericpan64@gatech.edu>
2019-10-08 00:01:45 -0500

    Starting mongoengine tutorial

---
+++ playgnd/mongoengine_tutorial/notes.py
@@ -0,0 +1,12 @@
+
+# Notes for Mongoengine
+"""
+Section 4-1 Tutorial (page 13):
+    https://buildmedia.readthedocs.org/media/pdf/mongoengine-odm/latest/mongoengine-odm.pdf
+
+"""
+
+
+from mongoengine import *
+
+connect('test-DB') # connects mongoengine to existing mongodb database
