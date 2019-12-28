323dcc94da9f3e6748d3fa1bb93899aa451f49ee
ericpan64 <ericpan64@gatech.edu>
2019-12-27 16:04:24 -0800

    Removed zwWord

--- server/app/models/zwChars.py
+++ server/app/models/zwChars.py
@@ -9,17 +9,6 @@ from mongoengine import Document, EmbeddedDocumentField, StringField
 from mongoengine import EmbeddedDocument, BooleanField, DictField, EmbeddedDocumentListField, DateTimeField
 import datetime
 
-class zwWord(EmbeddedDocument):
-    """ Represents single chinese character part of a phrase """
-    # Required
-    word = StringField(required=True) # raw text received
-    is_simplified = BooleanField(required=True) # True if simplified, else is traditional
-    pinyin = StringField(required=True)
-
-    # Optional
-    definition = StringField()
-    radicals = DictField()
-
 class zwPhrase(EmbeddedDocument):
     """ Represents Chinese phrase saved by user. Only exists within zwUser."""
     # Required
@@ -33,6 +22,7 @@ class zwPhrase(EmbeddedDocument):
     context_title = DictField() # Store context for ALL occurrences of the phrase
     context_URL = DictField()
     date_saved = DateTimeField(default=datetime.datetime.now)
+    radicals = DictField()
 
     comment = StringField() # Optional user-added comment
 
