021898cf9525dd1a0589c28cd20c1f172175db82
ericpan64 <ericpan64@gatech.edu>
2020-01-29 16:50:58 -0600

    Removing unused files
    Migrating data from config.py and utils.py to stanford.py (where data
    is called)

--- server/app/lib/stanford.py
+++ server/app/lib/stanford.py
@@ -1,17 +1,55 @@
 """
-Author: Martin Kess
-Description: Runs CoreNLP/PoS tagger. Calls utils.py
+Authors: Martin Kess, Eric Pan
+Description: Runs CoreNLP/PoS tagger using sockets as context managers
+    CoreNLP must be running on separate port on the server
+    Socket sends data back/forth
+        Socket calls are implemented as a context manager so each call is self-contained
+        (i.e. connections are opened/closed properly)
+
+Socket Reference:
+    https://www.geeksforgeeks.org/socket-programming-python/
 """
 
 # -*- coding: utf-8 -*-
 
-from utils import recv_all, socketcontext
-from app import app
+from contextlib import contextmanager
 import socket
 
-SEGMENT_SERVER_PORT = app.config['SEGMENT_SERVER_PORT']
-POS_SERVER_PORT     = app.config['POS_SERVER_PORT']
+# === Port Definitions ===
+SEGMENT_SERVER_PORT = 8083
+POS_SERVER_PORT     = 8084
 
+# === Context Manager Utility Functions  ===
+# from:
+@contextmanager
+def socketcontext(*args, **kw):
+    """
+    Utility function to initiate/close a socket (this isn't implemented natively in Python's socket library)
+    :returns: Generator for this function
+    Ref on code: http://stackoverflow.com/a/16772520
+    Ref on yield: https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
+    """
+    s = socket.socket(*args, **kw)
+    try:
+        yield s
+    finally:
+        s.close()
+
+def recv_all(sock):
+    """
+    Given server socket, receive ALL of the data! (on port 4096)
+    :param sock: Socket object (server)
+    :returns: data received by socket (string)
+    """
+    data = []
+    while True:
+        r = sock.recv(4096) # Receives data on this port
+        if not r:
+            break
+        data.append(r)
+    return ''.join(data)
+
+# === CoreNLP Segmenter API Calls ===
 def segment_text(text):
     ''' Given a sentence, insert spaces as words '''
     with socketcontext(socket.AF_INET, socket.SOCK_STREAM) as sock:
