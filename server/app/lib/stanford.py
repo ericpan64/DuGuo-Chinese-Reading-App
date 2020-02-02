ca086857d239d78bf95738ac43354a12ec84d012
ericpan64 <ericpan64@gatech.edu>
2020-02-01 23:57:17 -0600

    Adding initial coreNLP notes

--- server/app/lib/stanford.py
+++ server/app/lib/stanford.py
@@ -1,5 +1,5 @@
 """
-Authors: Martin Kess, Eric Pan
+Authors: Eric Pan
 Description: Runs CoreNLP/PoS tagger using sockets as context managers
     CoreNLP must be running on separate port on the server
     Socket sends data back/forth
@@ -14,6 +14,53 @@ Socket Reference:
 
 from contextlib import contextmanager
 import socket
+
+# === Stanford NLP API Definition ===
+# Adopted from blog post: https://www.khalidalnajjar.com/how-to-setup-and-use-stanford-corenlp-server-with-python/
+# Run shell command first
+from stanfordcorenlp import StanfordCoreNLP
+import json
+
+class StanfordNLP:
+    def __init__(self, host='http://localhost', port=9000):
+        self.nlp = StanfordCoreNLP(host, port=port,
+                                   timeout=30000)  # , quiet=False, logging_level=logging.DEBUG)
+        self.props = {
+            'annotators': 'tokenize,ssplit,pos,lemma,ner,parse,depparse,dcoref,relation',
+            'pipelineLanguage': 'zh',
+            'outputFormat': 'json'
+        }
+
+    def word_tokenize(self, sentence):
+        return self.nlp.word_tokenize(sentence)
+
+    def pos(self, sentence):
+        return self.nlp.pos_tag(sentence)
+
+    def ner(self, sentence):
+        return self.nlp.ner(sentence)
+
+    def parse(self, sentence):
+        return self.nlp.parse(sentence)
+
+    def dependency_parse(self, sentence):
+        return self.nlp.dependency_parse(sentence)
+
+    def annotate(self, sentence):
+        return json.loads(self.nlp.annotate(sentence, properties=self.props))
+
+    @staticmethod
+    def tokens_to_dict(_tokens):
+        tokens = defaultdict(dict)
+        for token in _tokens:
+            tokens[int(token['index'])] = {
+                'word': token['word'],
+                'lemma': token['lemma'],
+                'pos': token['pos'],
+                'ner': token['ner']
+            }
+        return tokens
+
 
 # === Port Definitions ===
 SEGMENT_SERVER_PORT = 8083
@@ -50,19 +97,23 @@ def recv_all(sock):
     return ''.join(data)
 
 # === CoreNLP Segmenter API Calls ===
-def segment_text(text):
-    ''' Given a sentence, insert spaces as words '''
-    with socketcontext(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect(('localhost', SEGMENT_SERVER_PORT))
-        sock.sendall(text.encode('utf-8') + '\n')
-        return recv_all(sock).decode('utf-8').strip()
 
+# TODO Reimplement using CoreNLP API
+def segment_text(text):
+    pass
+#     ''' Given a sentence, insert spaces as words '''
+#     with socketcontext(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#         sock.connect(('localhost', SEGMENT_SERVER_PORT))
+#         sock.sendall(text.encode('utf-8') + '\n')
+#         return recv_all(sock).decode('utf-8').strip()
+#
 def get_parts_of_speech(sentence):
-    ''' Given a segmented sentence, receive the sentence back with parts of
-        speech tagged in XML form. '''
-    with socketcontext(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect(('localhost', POS_SERVER_PORT))
-        sock.sendall(sentence.encode('utf-8') + '\n')
-        return recv_all(sock).decode('utf-8').strip()
+    pass
+#     ''' Given a segmented sentence, receive the sentence back with parts of
+#         speech tagged in XML form. '''
+#     with socketcontext(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#         sock.connect(('localhost', POS_SERVER_PORT))
+#         sock.sendall(sentence.encode('utf-8') + '\n')
+#         return recv_all(sock).decode('utf-8').strip()
 
 
