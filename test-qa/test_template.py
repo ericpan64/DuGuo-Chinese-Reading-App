f0b578e0810c868eb479a2c5f1df30ee95569f72
Eric Pan <epan9@gatech.edu>
2020-04-01 15:52:30 -0500

    Creating template file

---
+++ test-qa/test_template.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestZhongwenPy(unittest.TestCase):
+    def setUp(self):
+        # do minimum necessary set-up to go through test cases
+        pass
+
+    def test_function_name():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
