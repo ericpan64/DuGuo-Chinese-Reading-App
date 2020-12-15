2edf4b6fe11475b42826f78481d522d8ff30fa3c
Eric Pan <ericpan64@gatech.edu>
2020-12-14 20:05:32 -0800

    Add JWT authentication

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use rocket::{
     request::Form,
     response::Redirect,
-    http::RawStr,
+    http::{RawStr, Cookies},
     State,
 };
 use rocket_contrib::templates::Template;
@@ -14,15 +14,26 @@ use ::duguo::*; // lib.rs
 
 /* GET */
 #[get("/")]
-fn index() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
+fn index(cookies: Cookies, db: State<Database>) -> Template {
+    let mut context: HashMap<&str, String> = HashMap::new();
+    let cookie_lookup = cookies.get(JWT_NAME);
+    let username_from_cookie = get_username_from_cookie(db.clone(), cookie_lookup);
+    match username_from_cookie {
+        Some(username) => { context.insert("username", username); },
+        None =>  {}
+    }
     return Template::render("index", context);
 }
 
 #[get("/login")]
-fn login() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
-    // TODO - check for JWT, and mention username in html if already logged-in
+fn login(cookies: Cookies, db: State<Database>) -> Template {
+    let mut context: HashMap<&str, String> = HashMap::new();
+    let cookie_lookup = cookies.get(JWT_NAME);
+    let username_from_cookie = get_username_from_cookie(db.clone(), cookie_lookup);
+    match username_from_cookie {
+        Some(username) => { context.insert("username", username); },
+        None =>  {}
+    }
     return Template::render("login", context);
 }
 
@@ -39,7 +50,7 @@ fn sandbox_view_doc(db: State<Database>, doc_id: &RawStr) -> Template {
     */
     let mut context: HashMap<&str, &str> = HashMap::new();
     // Note: var html will _eventually_ need to be html, though plaintext for now
-    let doc_id = convert_rawStr_to_String(doc_id);
+    let doc_id = convert_rawstr_to_string(doc_id);
     let html = match get_sandbox_document(db.clone(), doc_id) {
         Some(text) => {text},
         None => String::new()
@@ -50,6 +61,7 @@ fn sandbox_view_doc(db: State<Database>, doc_id: &RawStr) -> Template {
     return Template::render("reader", context);
 }
 
+// Note: these need to be here since they use Rocket macros
 /* POST Forms */
 #[derive(FromForm)]
 struct UserLoginForm<'f> {
@@ -71,33 +83,38 @@ struct TextForm<'f> {
 
 /* POST */
 #[post("/login", data = "<user_input>")]
-fn login_form(db: State<Database>, user_input: Form<UserLoginForm<'_>>) -> Template {
+fn login_form(mut cookies: Cookies, db: State<Database>, user_input: Form<UserLoginForm<'_>>) -> Redirect {
     /*
     TODO - in comments
     */
     let UserLoginForm { username, password } = user_input.into_inner();
-    let username = convert_rawStr_to_String(username);
-    let password = convert_rawStr_to_String(password);
+    let username = convert_rawstr_to_string(username);
+    let password = convert_rawstr_to_string(password);
 
-    let is_valid_password = check_password(db.clone(), username.clone(), password);
+    let is_valid_password = check_password(db.clone(), username.clone(), password.clone());
     let mut context = HashMap::new();
-    if is_valid_password {
-        // add login handling context!
-        // (figure-out how to handle user "logged-in" status)
-        return Template::render("index", context);
-    }
-    let password_incorrect_msg = format!("Incorrect password for {}. N incorrect attempts remaining for today", username);
-    context.insert("message", password_incorrect_msg);
-    // (record login attempt in database)
-    return Template::render("login", context);
+    let res_redirect = match is_valid_password {
+        true => {
+            let new_cookie = generate_http_cookie(username, password);
+            cookies.add(new_cookie);
+            Redirect::to(uri!(index))
+        },
+        false => {
+            // (record login attempt in database)
+            let password_incorrect_msg = format!("Incorrect password for {}. N incorrect attempts remaining for today", username);
+            context.insert("message", password_incorrect_msg);
+            Redirect::to(uri!(login))
+        }
+    };
+    return res_redirect;
 }
 
 #[post("/login-post", data = "<user_input>")]
 fn register_form(db: State<Database>, user_input: Form<UserRegisterForm<'_>>) -> Template {
     let UserRegisterForm { username, email, password } = user_input.into_inner();
-    let username = convert_rawStr_to_String(username);
-    let password = convert_rawStr_to_String(password);
-    let email = convert_rawStr_to_String(email);
+    let username = convert_rawstr_to_string(username);
+    let password = convert_rawstr_to_string(password);
+    let email = convert_rawstr_to_string(email);
 
     let new_user = User::new(username, password, email);
     let message = new_user.try_insert(db.clone()).unwrap();
@@ -113,7 +130,7 @@ fn sandbox_upload(db: State<Database>, user_text: Form<TextForm<'_>>) -> Redirec
     TODO - in comments
     */
     let TextForm { text } = user_text.into_inner();    
-    let text_as_string = convert_rawStr_to_String(text);
+    let text_as_string = convert_rawstr_to_string(text);
     let new_doc = SandboxDoc::new(text_as_string);
     let inserted_id = new_doc.try_insert(db.clone()).unwrap();
     // Redirect to URL with document ID
