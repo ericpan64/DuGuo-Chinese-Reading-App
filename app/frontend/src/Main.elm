de77908197d49e9d5272b9c2fa09d42e9b59c033
ericpan64 <ericpan64@gatech.edu>
2021-07-23 07:41:17 -0400

    Update repo structure
    - Split `/app` folder to `backend` and `frontend` directories
    - Updated corresponding Dockerfiles

---
+++ app/frontend/src/Main.elm
@@ -0,0 +1,19 @@
+module Main exposing (..)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+type Msg = Increment | Decrement
+
+update msg model = 
+    case msg of
+        Increment -> model + 1
+        Decrement -> model - 1
+    
+view model =
+    div [] [
+        button [ onClick Decrement ] [ text "Decrement" ]
+        , div [] [ text (String.fromInt model)]
+        , button [ onClick Increment ] [ text "Increment"]
+    ]
+main = Browser.sandbox { init = 0, update = update, view = view}
