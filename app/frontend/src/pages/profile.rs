bcf79141171854498bdcd586089f4ce72af3300f
Eric Pan <ericpan64@gatech.edu>
2021-10-05 09:51:16 -0400

    Update user profile page view

--- app/frontend/src/pages/profile.rs
+++ app/frontend/src/pages/profile.rs
@@ -44,100 +44,75 @@ impl Component for Profile {
                 <div class="container">
                     <h1>{self.uid.as_str()}</h1>
                         <hr/>
-                        {self.view_settings()}
-                        <br/>
-                        <br/>
-                        {self.view_upload()}
-                        {self.view_tables()}
+                        <h5>{"User Settings"}</h5>
+                        <ul class="nav nav-pills">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">{format!("Phonetic: {}", self.phonetic_type.as_str())}</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdatePhonetic(Phonetic::Pinyin))>{"View Pinyin"}</a></li>
+                                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdatePhonetic(Phonetic::Zhuyin))>{"View Zhuyin (Bopomofo)"}</a></li>    
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="ml-2 nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">{format!("Charset: {}", self.cn_type.as_str())}</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdateCnType(CnType::Simplified))>{"View Simplified"}</a></li>
+                                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdateCnType(CnType::Traditional))>{"View Traditional"}</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                        <h4 class="mt-4">{"Upload Document"}</h4>
+                        <hr/>
+                        <h5>{"Parse from URL"}</h5>
+                        <form class="form" action="/api/upload" id="user-url-form" onsubmit=self.link.callback(|_| Msg::SwitchToLoadingButton) method="POST">
+                            <input type="text" name="url" placeholder="Document URL" required=true/>
+                            <button id="url-upload-button" class="ml-2 btn btn-outline-primary" type="submit">{"Upload URL"}</button>
+                        </form>
+                        <h5>{"Copy & Paste"}</h5>
+                        <form class="form" action="/api/upload" id="user-doc-form" onsubmit=self.link.callback(|_| Msg::SwitchToLoadingButton) method="POST">
+                            <input class="mt-2" type="text" name="title" placeholder="Document Title" required=true/><br/>
+                            <input class="mt-2" type="text" name="source" placeholder="Document Source (optional)"/>
+                            <textarea class="mt-2 form-control" name="body" form="user-doc-form" rows="2" cols="15" required=true></textarea>
+                            <button id="doc-upload-button" class="mt-2 btn btn-outline-primary" type="submit">{"Upload Text"}</button>
+                        </form>
+                        <h4 class="mt-4">{"View Documents + Vocab"}</h4>
+                        <hr/>
+                        <ul class="nav nav-pills nav-fill" id="pills-tabs" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="pills-docs-tab" data-bs-toggle="pill" data-bs-target="#pills-docs" role="tab" aria-controls="pills-docs" aria-selected="true">{"View Documents"}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="pills-vocab-tab" data-bs-toggle="pill" data-bs-target="#pills-vocab" role="tab" aria-controls="pills-vocab" aria-selected="false">{"View Vocab"}</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="pills-tabContent-userContent">
+                            <div class="tab-pane fade show active pt-3" id="pills-docs" role="tabpanel" aria-labelledby="pills-docs-tab">
+                                <table id="doc-table" class="table table-hover">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th>{"Title"}</th><th>{"Source"}</th><th>{"Created On"}</th><th>{"Delete"}</th>
+                                        </tr>
+                                    </thead>
+                                    <caption hidden=true>{"List of your saved documents."}</caption>
+                                </table>
+                                <button class="btn btn-outline-primary" onclick=self.link.callback(|_| Msg::DownloadDocs)>{"Export Documents as .csv"}</button>
+                            </div>
+                            <div class="tab-pane fade pt-3" id="pills-vocab" role="tabpanel" aria-labelledby="pills-vocab-tab">
+                                <table id="vocab-table" class="table table-hover">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th>{"Phrase"}</th><th>{"Saved From"}</th><th>{"Saved On"}</th><th>{"Radical Map"}</th><th>{"Delete"}</th>
+                                        </tr>
+                                    </thead>
+                                    <caption hidden=true>{"List of your saved vocabulary."}</caption>
+                                </table>
+                                <button class="btn btn-outline-primary" onclick=self.link.callback(|_| Msg::DownloadVocab)>{"Export Vocab as .csv"}</button>
+                            </div>
+                        </div>
                 </div>
             </div>
         </header>
-        }
-    }
-}
-
-impl Profile {
-    fn view_settings(&self) -> Html {
-        html! {
-            <>
-                <h5>{"Settings"}</h5>
-                <button class="btn btn-primary dropdown-toggle" id="phonetic-setting" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {self.phonetic_type.as_str()}
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdatePhonetic(Phonetic::Pinyin))>{"View Pinyin"}</a></li>
-                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdatePhonetic(Phonetic::Zhuyin))>{"View Zhuyin (Bopomofo)"}</a></li>
-                </ul>
-                <button class="btn btn-primary dropdown-toggle" id="char-setting" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {self.cn_type.as_str()}
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdateCnType(CnType::Simplified))>{"View Simplified"}</a></li>
-                    <li><a class="dropdown-item" onclick=self.link.callback(|_| Msg::UpdateCnType(CnType::Traditional))>{"View Traditional"}</a></li>
-                </ul>
-            </>
-        }
-    }
-    fn view_upload(&self) -> Html {
-        html! {
-            <>
-                <h4>{"Upload Document"}</h4>
-                <hr/>
-                <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link" id="pills-url-tab" data-toggle="pill" href="#pills-url" role="tab" aria-controls="pills-vocab" aria-selected="false">{"Parse from URL"}</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active" id="pills-cp-tab" data-toggle="pill" href="#pills-cp" role="tab" aria-controls="pills-docs" aria-selected="true">{"Copy & Paste"}</a>
-                    </li>
-                    <div class="tab-content" id="pills-tabContent-userContent">
-                        <div class="tab-pane fade show active pt-3" id="pills-url" role="tabpanel" aria-labelledby="pills-url-tab">
-                            <h5>{"Parse from URL"}</h5>
-                            <form class="form" action="/api/upload" id="user-url-form" onsubmit=self.link.callback(|_| Msg::SwitchToLoadingButton) method="POST">
-                                <input type="text" name="url" placeholder="Document URL" required=true/>
-                                <button id="url-upload-button" class="btn btn-outline-primary" type="submit">{"Upload URL"}</button>
-                            </form>
-                        </div>
-                        <div class="tab-pane fade pt-3" id="pills-cp" role="tabpanel" aria-labelledby="pills-cp-tab">
-                            <h5>{"Copy & Paste"}</h5>
-                            <form class="form" action="/api/upload" id="user-doc-form" onsubmit=self.link.callback(|_| Msg::SwitchToLoadingButton) method="POST">
-                                <input type="text" name="title" placeholder="Document Title" required=true/><br/>
-                                <input type="text" name="source" placeholder="Document Source (optional)"/>
-                                <button id="doc-upload-button" class="btn btn-outline-primary" type="submit">{"Upload Text"}</button>
-                                <textarea class="form-control" name="body" form="user-doc-form" rows="2" cols="35" required=true></textarea>
-                            </form>
-                        </div>
-                    </div>
-                </ul>
-            </>
-        }
-    }
-    fn view_tables(&self) -> Html {
-        html! {
-            <>
-                <h4>{"View Documents + Vocab"}</h4>
-                <hr/>
-                <ul class="nav nav-pills nav-fill" id="pills-tab-userContent" role="tablist">
-                    <li class="nav-item">
-                    <a class="nav-link active" id="pills-docs-tab" data-toggle="pill" href="#pills-docs" role="tab" aria-controls="pills-docs" aria-selected="true">{"View Documents"}</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" id="pills-vocab-tab" data-toggle="pill" href="#pills-vocab" role="tab" aria-controls="pills-vocab" aria-selected="false">{"View Vocab"}</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="pills-tabContent-userContent">
-                    <div class="tab-pane fade show active pt-3" id="pills-docs" role="tabpanel" aria-labelledby="pills-docs-tab">
-                        {"{{ doc_table | safe }}"}
-                        {"# TODO: make this a modal popup w/ option to disable in future #"}
-                        {"# <p><strong>*Note*</strong>: Documents are linked to Saved Vocab, so deleting a Document also deletes Vocab saved from that Document.</p> #"}
-                        <button class="btn btn-outline-primary" onclick=self.link.callback(|_| Msg::DownloadDocs)>{"Export Documents as .csv"}</button>
-                    </div>
-                    <div class="tab-pane fade pt-3" id="pills-vocab" role="tabpanel" aria-labelledby="pills-vocab-tab">
-                        {"{{ vocab_table | safe }}"}
-                        <button class="btn btn-outline-primary" onclick=self.link.callback(|_| Msg::DownloadVocab)>{"Export Vocab as .csv"}</button>
-                    </div>
-                </div>
-            </>
         }
     }
 }
